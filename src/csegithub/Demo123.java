c5bc259a497bc1bacc6bbaeb10fcc9da64708dfe
Durga prasad <Durga prasad@LAPTOP-LN9V9DGH>
2022-10-17 09:58:52 +0530

    first commit

---
+++ src/csegithub/Demo123.java
@@ -0,0 +1,11 @@
+package csegithub;
+
+public class Demo123 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Devops lab");
+
+	}
+
+}
